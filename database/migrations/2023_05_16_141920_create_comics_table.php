8f027b70cbe53ba611050a85ca62c427d399b7e0
MatteoBassanin <matteo.bassanin@gmail.com>
2023-05-16 17:24:58 +0200

    stupid dollars finally seeded db

--- database/migrations/2023_05_16_141920_create_comics_table.php
+++ database/migrations/2023_05_16_141920_create_comics_table.php
@@ -16,13 +16,11 @@ return new class extends Migration
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('description');
             $table->string('thumb');
-            $table->decimal('price',4,2);
+            $table->string('price');
+            $table->string('series');
             $table->date('sale_date');
             $table->string('type');
-            $table->string('artists');
-            $table->string('writers');
             $table->timestamps();
         });
     }
--- database/seeders/ComicTableSeeder.php
+++ database/seeders/ComicTableSeeder.php
@@ -21,14 +21,11 @@ class ComicTableSeeder extends Seeder
 
             $newComic = new Comic();
             $newComic->title = $comic['title'];
-            $newComic->description = $comic['description'];
             $newComic->thumb = $comic['thumb'];
             $newComic->price = $comic['price'];
             $newComic->series = $comic['series'];
             $newComic->sale_date = $comic['sale_date'];
             $newComic->type = $comic['type'];
-            $newComic->artists = $comic['artists'];
-            $newComic->writers = $comic['writers'];
             $newComic->save();
             
 
