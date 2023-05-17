0f852459932f9a9fe77f6d8d95d22b144ecd8d13
MatteoBassanin <matteo.bassanin@gmail.com>
2023-05-17 17:12:24 +0200

    implented bonus

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,3 +4,12 @@ import * as bootstrap from 'bootstrap';
 import.meta.glob([
     '../img/**'
 ])
+
+
+let deleting = document.querySelector('.delete_button');
+
+function Doublecheck() {
+    prompt("sei sicuro ?");
+}
+
+deleting.addEventListener(click, Doublecheck, false);
--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -19,7 +19,35 @@
                         <form action="{{ route('comics.destroy', ['comic' => $comic->id]) }}" method="POST">
                             @csrf
                             @method('DELETE')
-                            <button type="submit" class="btn btn-danger">DELETE</button>
+                            <!-- Button trigger modal -->
+                            <button type="submit" class="btn btn-primary" data-bs-toggle="modal"
+                                data-bs-target="#staticBackdrop" data-bs-target="#modal{{ $comic->id }}">
+                                DELETE
+                            </button>
+
+                            <!-- Modal -->
+                            <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false"
+                                tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                aria-label="Close"></button>
+                                        </div>
+                                        <div class="modal-body">
+                                            ...
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary"
+                                                data-bs-dismiss="modal">Close</button>
+                                            <button type="submit" class="btn btn-danger delete_button">DELETE</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            {{-- <button type="submit" class="btn btn-danger delete_button">DELETE</button> --}}
                         </form>
                     </div>
                 </div>
